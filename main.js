7e7a7ef6b85f353d8c8616fa7fb989823a5f4b45
NibblesOnToes <701389@pdsb.net>
2022-05-31 20:42:18 -0400

    trying to read the file in extension environment

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 function firstlast(word){
     const fs = require('fs');
     var filter = [];
-    var content = fs.readFileSync("./words.txt").toString().split("\n");
-    for (i in content){
-        filter.push(content[i]);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', chrome.runtime.getURL('words.txt'), true);
+    for (i in xhr.responseText){
+        filter.push(xhr.responseText[i]);
     }
     console.log(filter);
-    for (let i=0; i<filter.length; i++){
-          if (word.toLowerCase == filter[i]){
-              word=filter[i][0];
-              for (let x=0; x<filter[i].length-1; x++){
-                  word+="*";
-              }
-            word+=filter[i][filter[i].length];
-          }  
-          console.log(i);
+    for (let i=0; i<filter.length; i++)
+    {
+        if (word.toLowerCase() == filter[i])
+        {
+            word=filter[i][0];
+            for (let x=0; x<filter[i].length-1; x++){
+                word+="*";
+            }
+            word+=filter[i][filter[i].length-1];
+        }  
     }
     return word;
 }
-console.log(firstlast("woman"));
+//document.write("hi");
+x=firstlast("woman");
+document.write(x);
